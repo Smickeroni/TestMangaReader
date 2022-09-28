6cbcbd6d1a09384d09a68da48da491d14fa40a96
Eric Le <smickeroni@gmail.com>
2022-09-27 23:01:32 -0700

    fully functional now, just fix css

--- frontEnd/frontend.js
+++ frontEnd/frontend.js
@@ -1,6 +1,7 @@
 // Constants
 const base_url = window.location.href;
 const data_port = base_url + 'data'
+const imgPanel = document.getElementById("Panel");
 
 // Code
     // Post
@@ -18,19 +19,33 @@ const post = (url, data) => {
         body: JSON.stringify(data)
     })
 }
-
-    // Page scrolling
-
     // Changing visuals
+var currentPage = 0;
+var pages = {};
 
+const setupPages = (pagelist) => {
+    currentPage = 0;
+    pages = pagelist;
+    imgPanel.setAttribute("src",pages[currentPage]);
+}
+
+const changePage = (cur_page) => {
+    imgPanel.setAttribute("src",pages[cur_page]);
+}
     // Key presses
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'ArrowRight':
-            console.log("go forward a page");
+            if (currentPage < pages.length) {
+                currentPage++;
+                changePage(currentPage);
+            }
             return;
         case 'ArrowLeft':
-            console.log("go back a page");
+            if (currentPage > 0) {
+                currentPage--;
+                changePage(currentPage);
+            }
             return;
         default:
             return;
@@ -41,9 +56,11 @@ const sendSearchRequest = async () => {
     const Name = document.getElementById("Name");
     const Chapter = document.getElementById("Chapter");
     console.log("Sending search request for \nManga: " + Name.value + "\nChapter: " + Chapter.value);
-    const res = post(data_port, {
+    const res = await post(data_port, {
         mangaName: Name.value,     
         mangaChapter: Chapter.value 
    });
-   console.log((await res).text());
+   const url_list = await res.text();
+   const pages = JSON.parse(url_list);
+   setupPages(pages.pages_urls);
 }
